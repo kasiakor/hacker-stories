2429518a2134251fa604b78cf7fe90b3ca572fae
kasia.kordys <kasia.kordys@gmail.com>
2023-05-05 19:16:34 +0300

    add eslint

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -1,14 +1,11 @@
-// eslint-disable-next-line no-unused-vars
 import * as React from "react";
 
-function getTitle(title) {
-  return title;
-}
+const title = "React";
 
 function App() {
   return (
     <div>
-      <h1>Hello {getTitle("React")}</h1>
+      <h1>Hello React</h1>
     </div>
   );
 }
