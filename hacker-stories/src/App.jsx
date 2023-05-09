21c138c47812079133c18a6eb2d64da906dec3e2
kasia.kordys <kasia.kordys@gmail.com>
2023-05-09 19:06:24 +0300

    display list of items using map

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -1,11 +1,41 @@
+// eslint-disable-next-line no-unused-vars
 import * as React from "react";
 
-const title = "React";
+const list = [
+  {
+    color: "purple",
+    type: "minivan",
+    capacity: 7,
+    cardId: 1,
+  },
+  {
+    color: "red",
+    type: "sedan",
+    capacity: 4,
+    cardId: 2,
+  },
+  {
+    color: "blue",
+    type: "cabrio",
+    capacity: 2,
+    cardId: 3,
+  },
+];
 
 function App() {
   return (
     <div>
-      <h1>Hello React</h1>
+      <h1>My car collection</h1>
+      <hr />
+      <ul>
+        {list.map((car) => (
+          <li key={car.cardId}>
+            <span>{car.type}, </span>
+            <span>{car.color}, </span>
+            <span>{car.capacity}</span>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
