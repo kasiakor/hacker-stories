4d77722ca1644f2880d28f483f180998e6f746c0
kasia.kordys <kasia.kordys@gmail.com>
2023-06-22 23:25:28 +0300

    pass props to List Component

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -1,39 +1,41 @@
 // eslint-disable-next-line no-unused-vars
 import * as React from "react";
 
-const list = [
-  {
-    color: "purple",
-    type: "minivan",
-    capacity: 7,
-    cardId: 1,
-  },
-  {
-    color: "red",
-    type: "sedan",
-    capacity: 4,
-    cardId: 2,
-  },
-  {
-    color: "blue",
-    type: "cabrio",
-    capacity: 2,
-    cardId: 3,
-  },
-];
+const App = () => {
+  const stories = [
+    {
+      color: "purple",
+      type: "minivan",
+      capacity: 7,
+      cardId: 1,
+    },
+    {
+      color: "red",
+      type: "sedan",
+      capacity: 4,
+      cardId: 2,
+    },
+    {
+      color: "blue",
+      type: "cabrio",
+      capacity: 2,
+      cardId: 3,
+    },
+  ];
+  return (
+    <div>
+      <h1>My car collection</h1>
+      <Search />
+      <hr />
+      <List list={stories} />
+    </div>
+  );
+};
 
-const App = () => (
-  <div>
-    <h1>My car collection</h1>
-    <Search />
-    <hr />
-    <List />
-  </div>
-);
-
-const List = () => (
+const List = (props) => (
   <ul>
-    {list.map((car) => (
+    {/* eslint-disable-next-line react/prop-types */}
+    {props.list.map((car) => (
       <li key={car.cardId}>
         <span>{car.type}, </span>
         <span>{car.color}, </span>
